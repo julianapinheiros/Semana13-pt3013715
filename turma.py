e5de2136bb0e8699e98bb0f3332e01632433c533
Juliana Silva <julianasilva180198@gmail.com>
2023-11-16 19:59:58 -0300

    Create turma.py

---
+++ turma.py
@@ -0,0 +1,20 @@
+class Turma:
+  def __init__(self):
+    self.turma = [];
+    self.menorNota = float('inf') 
+    self.maiorNota = float('-inf') 
+
+  def cadastrarAlunos(self, alunos):
+    for aluno in alunos:
+      if aluno.nota < 0:
+        raise ValueError('A nota não pode ser menor que 0')
+      self.turma.append(aluno)
+      if aluno.nota < self.menorNota:
+        self.menorNota = aluno.nota
+      if aluno.nota > self.maiorNota:
+        self.maiorNota = aluno.nota               
+
+  def mostrarAlunos(self):  
+    print('Quantidade de alunos:', len(self.turma));
+    for x in self.turma:      
+      print(x.mostrarAluno());
